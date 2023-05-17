5a38c79fe9011faf2261419561f6d171ae78d67b
Guilherme-Scarano <guilhermeaphs@gmail.com>
2023-05-17 17:53:50 -0300

    função compartilhar

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -6,7 +6,6 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardActions from "@mui/material/CardActions";
-import { Avatar, CardHeader } from "@mui/material";
 
 const img_styles = {
   media: {
@@ -14,43 +13,47 @@ const img_styles = {
     width: 270,
     paddingLeft: 50,
     paddingRight: 50,
-    paddingTop: 10
-  }
+    paddingTop: 10,
+  },
 };
 
-const PeopleCard = ({ name, turma, email, photoUrl, linkedinUrl, compartilhar}) => {
+const PeopleCard = ({ name, turma, email, photoUrl, linkedinUrl }) => {
+  const handleShare = async (name, url) => {
+    try {
+      await navigator.share({
+        title: "Compartilhar",
+        text: `Confira ${name} no LinkedIn!`,
+        url: url,
+      });
+      console.log("Conteúdo compartilhado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao compartilhar:", error);
+    }
+  };
+
   return (
     <Card sx={{ minWidth: 210, maxWidth: 380, minHeight: 420 }} className="PeopleCard">
       <a href={`mailto:${email}`} target="_blank" rel="noreferrer">
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          style={img_styles.media}
-          image={photoUrl}
-          alt={name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" color="blue" textAlign="center">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            <b>Turma:</b> {turma} <br />
-            <b>Email:</b> {email}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+        <CardActionArea>
+          <CardMedia component="img" style={img_styles.media} image={photoUrl} alt={name} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" color="blue" textAlign="center">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <b>Turma:</b> {turma} <br />
+              <b>Email:</b> {email}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </a>
       <CardActions sx={{ justifyContent: "center" }}>
-      <a href={compartilhar} target="_blank">
-    <Button size="small" color="primary">
-      Compartilhar
-    </Button>
-  </a>
-  <a href={linkedinUrl} target="_blank">
-    <Button size="small">
-      LinkedIn
-    </Button>
-  </a>
+        <Button size="small" color="primary" onClick={() => handleShare(name, linkedinUrl)}>
+          Compartilhar
+        </Button>
+        <a href={linkedinUrl} target="_blank">
+          <Button size="small">LinkedIn</Button>
+        </a>
       </CardActions>
     </Card>
   );
