acfa8feba7fbd223629eaf0ceff8a9794a382225
Guilherme-Scarano <guilhermeaphs@gmail.com>
2023-06-02 19:15:48 -0300

    novo filtro

--- src/pages/PeoplePage.jsx
+++ src/pages/PeoplePage.jsx
@@ -4,32 +4,40 @@ import PeopleCard from "../components/PeopleCard";
 import jsonData from "./database.json";
 
 const PeoplePage = () => {
-  const [searchTerm, setSearchTerm] = useState(""); // State for the search term
+  const [searchTerm, setSearchTerm] = useState(""); // Estado para o termo de pesquisa
+  const [filterYear, setFilterYear] = useState(""); // Estado para o ano do filtro
   const peopleData = jsonData.people;
 
-  // Filter the data based on the search term
+  // Filtra os dados com base no termo de pesquisa e no ano do filtro
   const filteredData = peopleData.filter((person) => {
-    const values = Object.values(person).map((value) =>
-      value.toString().toLowerCase()
-    );
-    return values.some((value) => value.includes(searchTerm.toLowerCase()));
+    const includesSearchTerm = person.name.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchesFilterYear = filterYear === "" || person.turma.toString() === filterYear;
+    return includesSearchTerm && matchesFilterYear;
   });
 
-  // Handle search term change
+  // Manipula a mudança no termo de pesquisa
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  // Manipula a mudança no ano do filtro
+  const handleFilterYearChange = (event) => {
+    setFilterYear(event.target.value);
+  };
+
+  // Obtém os anos únicos das turmas
+  const turmaYears = [...new Set(peopleData.map((person) => person.turma.toString()))];
 
   return (
     <div>
       <div style={{ marginBottom: "20px" }}>
         <input
           type="text"
-          placeholder="Search"
+          placeholder="Pesquisar"
           value={searchTerm}
           onChange={handleSearchChange}
           style={{
-            width: "100%",
+            width: "199vh",
             padding: "10px",
             fontSize: "16px",
             border: "1px solid #ccc",
@@ -37,6 +45,14 @@ const PeoplePage = () => {
             boxSizing: "border-box",
           }}
         />
+      </div>
+      <div style={{ marginBottom: "20px" }}>
+        <select value={filterYear} onChange={handleFilterYearChange}>
+          <option value="">Mostrar todos</option>
+          {turmaYears.map((year) => (
+            <option value={year} key={year}>{year}</option>
+          ))}
+        </select>
       </div>
       <Grid container spacing={3} paddingRight={"50px"} paddingTop={"50px"} paddingLeft={"50px"}>
         {filteredData.map((person, index) => (
@@ -57,4 +73,3 @@ const PeoplePage = () => {
 };
 
 export default PeoplePage;
-
