8fd1aa8f399f0ebe1d47ceba06540e6211a7a243
Luiz Felipe <luizfelipesralves@gmail.com>
2023-11-27 16:41:39 -0300

    update design

---
+++ src/components/Header.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import LoginPage from "./LoginPage";
+import logoImage from "../img/iflogobranco.png";
+
+const Header = ({ isLoggedIn, setIsLoggedIn, openAddStudentForm }) => {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+
+  const handleLoginButtonClick = () => {
+    setShowLoginForm(!showLoginForm);
+  };
+
+  const handleLoginFormClose = () => {
+    setShowLoginForm(false);
+  };
+
+  const handleLogout = () => {
+    setShowLoginForm(false);
+    setIsLoggedIn(false);
+    sessionStorage.setItem("isLoggedIn", "false");
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: "#333",
+        padding: "10px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        position: "flex",
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1000,
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={logoImage}
+          alt="IF Logo"
+          style={{ height: "40px", marginRight: "10px" }}
+        />
+        <span style={{ color: "#fff", fontSize: "18px" }}>Instituto Federal - Campus Machado</span>
+      </div>
+
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {!isLoggedIn && !showLoginForm && (
+          <button
+            onClick={handleLoginButtonClick}
+            style={{
+              background: "green",
+              color: "white",
+              border: "none",
+              padding: "10px 20px",
+              borderRadius: "4px",
+              cursor: "pointer",
+              marginRight: "4px"
+            }}
+          >
+            Login
+          </button>
+        )}
+
+        {isLoggedIn && (
+          <>
+            <button
+              onClick={handleLogout}
+              style={{
+                background: "red",
+                color: "white",
+                border: "none",
+                padding: "10px 20px",
+                borderRadius: "4px",
+                cursor: "pointer",
+                marginLeft: "10px",
+              }}
+            >
+              Sair
+            </button>
+
+            <button
+              onClick={openAddStudentForm}
+              style={{
+                background: "blue",
+                color: "white",
+                border: "none",
+                padding: "10px 20px",
+                borderRadius: "4px",
+                cursor: "pointer",
+                marginLeft: "10px",
+              }}
+            >
+              Adicionar Egresso
+            </button>
+          </>
+        )}
+      </div>
+
+      {showLoginForm && !isLoggedIn && (
+        <LoginPage setIsLoggedIn={setIsLoggedIn} onClose={handleLoginFormClose} />
+      )}
+    </div>
+  );
+};
+
+export default Header;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,28 +7,33 @@ const LoginPage = ({ setIsLoggedIn }) => {
   const handleLogin = () => {
     // Você pode adicionar lógica de autenticação aqui
     if (username === "admin" && password === "admin") {
-      setIsLoggedIn(true);
+      const isAuthenticated = true; // Substitua por sua lógica de autenticação real
+
+      if (isAuthenticated) {
+        setIsLoggedIn(true);
+        sessionStorage.setItem("isLoggedIn", "true");
+        // Não há necessidade de redirecionar, pois já está na mesma página
+      } else {
+        alert("Credenciais inválidas. Tente novamente.");
+      }
     } else {
       alert("Credenciais inválidas. Tente novamente.");
     }
   };
 
   return (
-    <div>
-      <h2 style={{ color: "aquamarine", marginBottom: "10px" }}>Login</h2>
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
       <input
         type="text"
         placeholder="Usuário"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        style={{ marginBottom: "25px" }}
       />
       <input
         type="password"
         placeholder="Senha"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        style={{ marginBottom: "25px" }}
       />
       <button
         onClick={handleLogin}
